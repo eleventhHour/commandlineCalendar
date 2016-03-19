57e95b110e7b6dbae5f8b4c518ffe844e99fe5a7
ARC SJSU <arc.sjsu@gmail.com>
2016-03-18 20:48:12 -0700

    some things added to event class.

--- src/Event.java
+++ src/Event.java
@@ -1,8 +1,42 @@
+import java.util.GregorianCalendar;
+
 /**
  * Created by branden on 3/12/2016.
  */
 public class Event {
 
+    enum MONTHS
+{
+	Jan, Feb, March, Apr, May, June, July, Aug, Sep, Oct, Nov, Dec;
+}
+enum DAYS
+{
+	Sun, Mon, Tue, Wed, Thur, Fri, Sat ;
+}
+    String title;
+    private GregorianCalendar eventDateStart;
+    private GregorianCalendar eventDateEnd;
+
+    public Event( String title, int year, int month, int dayOfMonth,
+                  int startHour, int startMinute, int endHour, int endMinute) {
+        this.title = title;
+        eventDateStart = new GregorianCalendar(year, month, dayOfMonth, startHour, startMinute);
+        if(endHour != -1){
+         eventDateEnd = new GregorianCalendar(year, month, dayOfMonth, endHour, endMinute);
+        }
+    }
+
+    public Event( String title, GregorianCalendar eventDateStart, GregorianCalendar eventDateEnd) {
+        this.title = title;
+        this.eventDateStart = eventDateStart;
+        this.eventDateEnd = eventDateEnd;
+    }
+
+    public Event ( String title, GregorianCalendar eventDateStart) {
+        this.title = title;
+        this.eventDateStart = eventDateStart;
+
+    }
     public void setDate () {}
 
     public void getTitle() {}
